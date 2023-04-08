036c96c8a152058c042a8c59fec3006a1119ad1b
Rick Heist <rick.heist@gmail.com>
2023-04-07 23:35:55 -0400

    continued working on js. options var is throwing error no matter where i put it

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('Great Success!');
 let startButton = document.createElement("button");
 let questionElement = document.getElementById("questions");
 let timerElement = document.querySelector('#time-seconds');
@@ -8,15 +7,32 @@ let score = 0;
 let scoreElement = document.querySelector('#score');
 let boxElement = document.querySelector("#coding-quiz");
 let timerInterval = 0;
+let timerStarted = false;
+let buttonContainer = document.querySelectorAll("button");
+
+
+
+for (i = 0; i < buttonContainer.length; i++) {
+    buttonContainer[i].addEventListener("click", function() {
+        if (this.textContent === currentQuestion.correct) {
+            score++;
+        } else {
+         time -=5
+        }
+        if (questionIndex === questions.length - 1) {
+            //endQuiz();
+        } else {
+            questionIndex++;
+            currentQuestion = questions[questionIndex];
+            options = currentQuestion.options;
+            showQuestion();
+        }
+    })
+}
 
 scoreElement.textContent = `score: ${score}`;
 
 timerElement.textContent = `time left: ${time}`;
-
-timerInterval = setInterval( function() {
-    time--;
-    timerElement.textContent= `time left: ${time}`;
-}, 1000)
 
 startButton.setAttribute("type", "button");
 startButton.classList.add("btn");
@@ -24,59 +40,61 @@ startButton.classList.add("btn-primary");
 startButton.textContent = "Start/Stop Button";
 
 document.querySelector("#main-space").appendChild(startButton);
-startButton.addEventListener("click", function(){
-    var state = document.querySelector("#time-seconds").dataset.state;
-    if(state === "started"){
-        document.querySelector("#time-seconds").dataset.state = "stopped";
-        clearInterval(timerInterval);
-    } else {
-        document.querySelector("#time-seconds").dataset.state = "started";
+startButton.addEventListener("click", function(event){
+    console.log(event)
+    if(!timerStarted){
+        timerStarted = true;
         time = 45;
         timerInterval = setInterval( function() {
             time--;
             timerElement.textContent = `time left: ${time}`;       
         }, 1000)
+    } else {
+       clearInterval(timerInterval);
+       timerStarted = false;      
     }
+    startQuiz()
 
 // add high score "local storage" lesson 04-22
 
 })
 
 
-/* function startQuiz(){
+
+ function startQuiz(){
+    let currentQuestion = questionBoxes[questionIndex];
+    let options = options = currentQuestion.options; //console logging error here ive moved these variable all over the code and it errors on different lines
     console.log("start quiz")
-    setInterval(startTimer, 1000)
+    questionIndex = 0;    
+    currentQuestion = questions[questionIndex];    
     showQuestion();
-    questionIndex++;
-} */
-//startButton.addEventListener("click", startQuiz);
+}
+    
+
+startButton.addEventListener("click", startQuiz);
 
 var questionBoxes = [
     {
-        questionHead: "Question 1",
-        questionText: "Question Text",
+        question: "Question 1 Text",
         options: ["Red", "Blue", "Yellow"],
         correct: "Blue",
         
     },
     
     { 
-        questionHead: "Question 2",
-        questionText: "Question Text",
+        question: "Question 2 Text",
         options: ["Red", "Blue", "Yellow"],
         correct: "Blue",
     },
 
     {
-        questionHead: "Question 3",
-        questionText: "Question Text",
+        question: "Question 3 Text",
         options: ["Red", "Blue", "Yellow"],
         correct: "Blue",
     },
 
     {
-        questionHead: "Question 4",
-        questionText: "Question Text",
+        question: "Question 4 Text",
         options: ["Red", "Blue", "Yellow"],
         correct: "Blue",
     },
@@ -85,13 +103,41 @@ var questionBoxes = [
 
 console.log(questionBoxes[0]);
 
+
 function showQuestion(){
-    questionElement.textContent = questionBoxes[questionIndex].questionHead; // wont display all content of object
-    for (i = 0; i < questionBoxes[questionIndex].options; i++){
-        console.log(questionBoxes[questionIndex].options);
-    }
+    questionElement.innerHTML = '';
+    var question = document.createElement('div');
+    question.textContent = currentQuestion.question;
+    questionElement.appendChild(question);1
+    let options = currentQuestion.options;
+    for (i = 0; i < currentQuestion.options.length; i++){
+        console.log(options);
+        var option = document.createElement('button');
+        option.textContent = currentQuestion.options[i];
+        questionElement.appendChild(option);
+
+        option.addEventListener("click", function(){
+            console.log(currentQuestion.correct);
+            if (this.textContent === currentQuestion.correct){
+                score++
+            } else {
+                time -= 5;
+            }
+        })
+         questionIndex++;
+         scoreElement.textContent = `score: ${score}`;
+         timerElement.textContent = `time left: ${time}`;
+         if (questionIndex >= questionBoxes.length) {
+            // end quiz
+         } else {
+            showQuestion();
+         }
+    };
 }
 
+/* function endQuiz() {
+    clearInterval(timerInterval);
+} */
 
 
 boxElement.style.backgroundColor = "white";
