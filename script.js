06479684f46b6d29dd0f75e40a84fe3b8fb955aa
Rick Heist <rick.heist@gmail.com>
2023-04-08 23:01:41 -0400

    debugged javascript with help, added game over and restart after timeout. fleshed out questions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let startButton = document.createElement("button");
 let questionElement = document.getElementById("questions");
 let timerElement = document.querySelector('#time-seconds');
 let questionIndex = 0;
-let time = 45;
+let time = 30;
 let score = 0;
 let scoreElement = document.querySelector('#score');
 let boxElement = document.querySelector("#coding-quiz");
@@ -10,7 +10,45 @@ let timerInterval = 0;
 let timerStarted = false;
 let buttonContainer = document.querySelectorAll("button");
 
-let currentQuestion;// both lines will cause console logging error here ive moved these variable all over the code and it errors on different lines
+var questionBoxes = [
+    {
+        question: "JavaScript variables can be defined with the following except: ",
+        options: ["var", "def", "const", "let"],
+        correct: "def",
+        
+    },
+    
+    { 
+        question: "CSS stylesheets should be linked in the...",
+        options: ["body", "form", "head", "header"],
+        correct: "head",
+    },
+
+    {
+        question: "In CSS an ID is called with what?",
+        options: [".", "by name", "id=", "#"],
+        correct: "#",
+    },
+
+    {
+        question: "How is strict equality expressed?",
+        options: ["=", "===", "+==", "==+"],
+        correct: "===",
+    },
+
+    {
+        question: "What is NOT a Pseudo Element Postion",
+        options: ["Global", "Fixed", "Static", "Absolute"],
+        correct: "Global",
+    },
+
+    {
+        question: "What does git provide?",
+        options: ["coding", "website", "crypto", "version control"],
+        correct: "version control",
+    },
+];
+
 let options = questionBoxes[questionIndex].options;
 
 for (i = 0; i < buttonContainer.length; i++) {
@@ -21,7 +59,7 @@ for (i = 0; i < buttonContainer.length; i++) {
          time -=5
         }
         if (questionIndex === questions.length - 1) {
-            //endQuiz();
+            endQuiz();
         } else {
             questionIndex++;
             currentQuestion = questions[questionIndex];
@@ -32,41 +70,35 @@ for (i = 0; i < buttonContainer.length; i++) {
 }
 
 scoreElement.textContent = `score: ${score}`;
-
 timerElement.textContent = `time left: ${time}`;
-
 startButton.setAttribute("type", "button");
 startButton.classList.add("btn");
 startButton.classList.add("btn-primary");
 startButton.textContent = "Start/Stop Button";
 
 document.querySelector("#main-space").appendChild(startButton);
-startButton.addEventListener("click", function(event){
-    console.log(event)
+startButton.addEventListener("click", function(){   
     if(!timerStarted){
         timerStarted = true;
-        time = 45;
+        time = 30;
         timerInterval = setInterval( function() {
             time--;
-            timerElement.textContent = `time left: ${time}`;       
+            timerElement.textContent = `time left: ${time}`;
+            checkTime()       
         }, 1000)
     } else {
        clearInterval(timerInterval);
        timerStarted = false;      
-    }
-    
+    }    
     startQuiz()
 
-// add high score "local storage" lesson 04-22
+// add high score "local storage" lesson 04-22 score + time remaining
 
 })
 
 
 
  function startQuiz(currentQuestion){
-    
-     
-    console.log("start quiz")
     questionIndex = 0;    
     currentQuestion = questions[questionIndex];    
     showQuestion();
@@ -74,50 +106,18 @@ startButton.addEventListener("click", function(event){
     
 
 startButton.addEventListener("click", startQuiz);
-
-var questionBoxes = [
-    {
-        question: "Question 1 Text",
-        options: ["Red", "Blue", "Yellow"],
-        correct: "Blue",
-        
-    },
-    
-    { 
-        question: "Question 2 Text",
-        options: ["Red", "Blue", "Yellow"],
-        correct: "Blue",
-    },
-
-    {
-        question: "Question 3 Text",
-        options: ["Red", "Blue", "Yellow"],
-        correct: "Blue",
-    },
-
-    {
-        question: "Question 4 Text",
-        options: ["Red", "Blue", "Yellow"],
-        correct: "Blue",
-    },
-];
-
-
-console.log(questionBoxes[0]);
-
+let currentQuestion = questionBoxes[questionIndex]
 
 function showQuestion(){
     questionElement.innerHTML = '';
     var question = document.createElement('div');
     question.textContent = currentQuestion.question;
-    questionElement.appendChild(question);1
+    questionElement.appendChild(question);
     let options = currentQuestion.options;
     for (i = 0; i < currentQuestion.options.length; i++){
-        console.log(options);
         var option = document.createElement('button');
         option.textContent = currentQuestion.options[i];
         questionElement.appendChild(option);
-
         option.addEventListener("click", function(){
             console.log(currentQuestion.correct);
             if (this.textContent === currentQuestion.correct){
@@ -125,22 +125,34 @@ function showQuestion(){
             } else {
                 time -= 5;
             }
+            questionIndex++;
+            if (questionIndex >= questionBoxes.length) {
+                endQuiz();
+            } else {
+                currentQuestion = questionBoxes[questionIndex];
+                showQuestion();
+            }
+            scoreElement.textContent = `score: ${score}`;
+            timerElement.textContent = `time left: ${time}`;
         })
-         questionIndex++;
-         scoreElement.textContent = `score: ${score}`;
-         timerElement.textContent = `time left: ${time}`;
-         if (questionIndex >= questionBoxes.length) {
-            // end quiz
-         } else {
-            showQuestion();
-         }
-    };
+    }             
+}// quiz needs to end when timer hits 0 even if questions arent answered. Game Over alert
+
+function checkTime() {
+    if (time === 0) {
+        endQuiz()
+    }
 }
 
-/* function endQuiz() {
+function endQuiz() {
     clearInterval(timerInterval);
-} */
-
+    var gameOver = document.createElement("div");
+    gameOver.innerHTML = `
+        <h2>GAME OVER</h2>
+        <p>Your Score is ${score}</p><button onclick="location.reload()">Restart</button></p>
+    `;
+    boxElement.appendChild(gameOver);
+}
 
 boxElement.style.backgroundColor = "white";
 var correctAnswer = ''
